6b0f58c3bc12ad4dbd492e84c26e0d2c80557002
Xiangpeng Hao <xha62@sfu.ca>
2019-12-17 22:32:43 -0800

    setup echarts

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,41 @@
   <div id="app">
     <HelloWorld msg="Welcome to Your Vue.js App" />
     <el-button type="primary">Primary</el-button>
+    <div id="chart" style="height: 500px; width:500px"></div>
   </div>
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
+import echarts from 'echarts'
 
 export default {
   name: "app",
   components: {
     HelloWorld
+  },
+  mounted() {
+
+    // initialize echarts instance with prepared DOM
+    let myChart = echarts.init(document.getElementById("chart"));
+    // draw chart
+    myChart.setOption({
+      title: {
+        text: "ECharts entry example"
+      },
+      tooltip: {},
+      xAxis: {
+        data: ["shirt", "cardign", "chiffon shirt", "pants", "heels", "socks"]
+      },
+      yAxis: {},
+      series: [
+        {
+          name: "sales",
+          type: "bar",
+          data: [5, 20, 36, 10, 10, 20]
+        }
+      ]
+    });
   }
 };
 </script>
