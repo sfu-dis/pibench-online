032e2a8c3ddc9ed1e4a989f35df859f168247550
Xiangpeng Hao <xha62@sfu.ca>
2019-12-20 07:19:39 -0800

    add vuex

--- src/main.js
+++ src/main.js
@@ -4,11 +4,13 @@ import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import 'element-ui/lib/theme-chalk/reset.css'
 import locale from 'element-ui/lib/locale/lang/en'
+import store from './store'
 
 
 Vue.config.productionTip = false
 Vue.use(ElementUI, { locale });
 
 new Vue({
-  render: h => h(App),
+  store,
+  render: h => h(App)
 }).$mount('#app')
