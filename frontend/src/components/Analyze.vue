cede15b8d84f199f4278b81a1f8ace205c99df38
Xiangpeng Hao <xha62@sfu.ca>
2020-08-12 18:54:35 -0700

    fix plotting

--- frontend/src/components/Analyze.vue
+++ frontend/src/components/Analyze.vue
@@ -10,8 +10,16 @@ Xiangpeng Hao <xiangpeng_hao@sfu.ca>
   <div>
     <el-card>
       <h1>All Benchmark Results</h1>
-      <el-table size="small" :data="benchmarkResults" @selection-change="handleSelectionChange">
-        <el-table-column fixed type="selection" width="55"></el-table-column>
+      <el-table
+        size="small"
+        :data="benchmarkResults"
+        @selection-change="handleSelectionChange"
+      >
+        <el-table-column
+          fixed
+          type="selection"
+          width="55"
+        ></el-table-column>
         <el-table-column
           v-for="item in tableColumns"
           :fixed="item['fixed']"
@@ -22,12 +30,22 @@ Xiangpeng Hao <xiangpeng_hao@sfu.ca>
         ></el-table-column>
       </el-table>
       <section style="margin-top:1em; margin-bottom: 2em;">
-        <el-button @click="deleteSelected" size="mini" plain type="danger">Delete Selected</el-button>
+        <el-button
+          @click="deleteSelected"
+          size="mini"
+          plain
+          type="danger"
+        >Delete Selected</el-button>
       </section>
       <section style="margin-top: 3em;">
         <el-form inline>
           <el-form-item label="X axis: ">
-            <el-select value-key="label" v-model="figureSelect.x" size="mini" placeholder="X axis">
+            <el-select
+              value-key="label"
+              v-model="figureSelect.x"
+              size="mini"
+              placeholder="X axis"
+            >
               <el-option
                 v-for="op in tableOptions"
                 :label="op.label"
@@ -37,7 +55,12 @@ Xiangpeng Hao <xiangpeng_hao@sfu.ca>
             </el-select>
           </el-form-item>
           <el-form-item label="Y axis: ">
-            <el-select value-key="label" v-model="figureSelect.y" size="mini" placeholder="Y axis">
+            <el-select
+              value-key="label"
+              v-model="figureSelect.y"
+              size="mini"
+              placeholder="Y axis"
+            >
               <el-option
                 v-for="op in tableOptions"
                 :label="op.label"
@@ -88,11 +111,11 @@ export default {
         {
           fixed: true,
           prop: "param.basic.wrapper",
-          label: "Wrapper"
+          label: "Wrapper",
         },
         {
           prop: "param.params.thread_cnt",
-          label: "Thread"
+          label: "Thread",
         },
         { prop: "param.params.distribution", label: "Distribution" },
         { prop: "param.params.read", label: "Read" },
@@ -102,29 +125,29 @@ export default {
         { prop: "param.params.key_size", label: "Key Size" },
         { prop: "param.params.value_size", label: "Value Size" },
         { prop: "param.params.op_cnt", label: "OP Count" },
-        { fixed: true, prop: "result.basics.Throughput", label: "Throughput" },
+        { fixed: true, prop: "result.results.throughput", label: "Throughput" },
         {
           hidden: true,
           type: "array",
-          prop: "result.basics.latency",
-          label: "Latency"
+          prop: "result.results.latency",
+          label: "Latency",
         },
         {
           hidden: true,
           type: "array",
-          prop: "result.basics.samplings",
-          label: "Sampling"
-        }
-      ]
+          prop: "result.results.samples",
+          label: "Sampling",
+        },
+      ],
     };
   },
   computed: {
     ...mapState(["benchmarkResults"]),
     tableColumns() {
-      return this.tableOptions.filter(item => {
+      return this.tableOptions.filter((item) => {
         return !item["hidden"];
       });
-    }
+    },
   },
   methods: {
     ...mapMutations(["addBenchmarkResult", "updateBenchmarkResults"]),
@@ -133,40 +156,43 @@ export default {
     },
     deleteSelected() {
       let self = this;
-      const newBenchResults = this.benchmarkResults.filter(item => {
+      const newBenchResults = this.benchmarkResults.filter((item) => {
         return self.currentSelected.indexOf(item) === -1;
       });
       this.updateBenchmarkResults(newBenchResults);
     },
     getLatencyOption(xValues, yValues) {
+      console.log(xValues, yValues);
+      let xAxisLabel = Object.keys(yValues[0]);
+
       const series = yValues.map((item, index) => {
         return {
           type: "line",
-          data: item.values,
-          name: `${this.figureSelect.x.label} ${xValues[index]}`
+          data: Object.values(item),
+          name: `${this.figureSelect.x.label} ${xValues[index]}`,
         };
       });
       const newOption = {
         xAxis: {
           name: "Sampling",
-          data: yValues[0].labels
+          data: xAxisLabel,
         },
         dataZoom: [
           {
             show: true,
             filterMode: "empty",
-            yAxisIndex: 0
-          }
+            yAxisIndex: 0,
+          },
         ],
         yAxis: {
           name: "Latency ",
           axisLabel: {
-            formatter: value => {
+            formatter: (value) => {
               return value / 1000 + " us";
-            }
-          }
+            },
+          },
         },
-        series: [...series]
+        series: [...series],
       };
       return newOption;
     },
@@ -174,17 +200,17 @@ export default {
       let series = {
         type: "line",
         data: yValues,
-        name: this.figureSelect.y.label
+        name: this.figureSelect.y.label,
       };
       const newOption = {
         xAxis: {
           name: this.figureSelect.x.label,
-          data: xValues
+          data: xValues,
         },
         yAxis: {
-          name: this.figureSelect.y.label
+          name: this.figureSelect.y.label,
         },
-        series
+        series,
       };
       return newOption;
     },
@@ -194,10 +220,10 @@ export default {
         this.$message("x type not supported");
         return;
       }
-      const xValues = this.currentSelected.map(item => {
+      const xValues = this.currentSelected.map((item) => {
         return get(item, self.figureSelect.x.prop);
       });
-      const yValues = this.currentSelected.map(item => {
+      const yValues = this.currentSelected.map((item) => {
         return get(item, self.figureSelect.y.prop);
       });
 
@@ -212,31 +238,31 @@ export default {
       options = {
         ...options,
         title: {
-          text: "Benchmark Result"
+          text: "Benchmark Result",
         },
         grid: { left: "15%", right: "15%" },
         tooltip: {
-          trigger: "axis"
+          trigger: "axis",
         },
         toolbox: {
           feature: {
             dataView: { show: true, readOnly: false, title: "Dataview" },
             restore: { show: true, title: "Restore" },
-            saveAsImage: { show: true, title: "Save As Image" }
-          }
-        }
+            saveAsImage: { show: true, title: "Save As Image" },
+          },
+        },
       };
       this.allFigures.push(cloneDeep(options));
 
-      this.$nextTick(function() {
+      this.$nextTick(function () {
         let dom = document.getElementById(
           "figure-" + (self.allFigures.length - 1)
         );
         let newChart = echarts.init(dom);
         newChart.setOption(options);
       });
-    }
-  }
+    },
+  },
 };
 </script>
 
